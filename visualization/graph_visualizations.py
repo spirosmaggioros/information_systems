51763c8b430a90d8e0e220326d67af2c1e2f4711
Reidi Pasai <reidipasai03@gmail.com>
2025-10-30 21:22:55 +0200

    [visualization] Bug fixed and undated visualization
    In the case of graph without node labels, the program crashed. Fixed the bug and also added color for the node labels to be beautifully plotted

--- visualization/graph_visualizations.py
+++ visualization/graph_visualizations.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
 import networkx as nx
+import matplotlib.cm as cm
 
 
-def plot_graph(graph: nx.Graph, node_labels: list = []) -> None:
+def plot_graph(graph: nx.Graph, node_labels: list = None) -> None:
     """
     Simple plot of a nx.Graph with node labels
+    - If no node_labels are given: nodes are blue and labeled by ID
+    - If node_labels are given: each label has a unique color
 
     :param graph: The input graph
     :type graph: nx.Graph
@@ -22,14 +25,45 @@ def plot_graph(graph: nx.Graph, node_labels: list = []) -> None:
 
     plot_graph(graphs[0], data["node_labels"])
     """
+    nodes = list(graph.nodes())
+    pos = nx.spring_layout(graph, seed=42)
 
-    nodes = graph.nodes()
+    if not node_labels:
+        labels = {n: str(n) for n in nodes}
+        node_colors = "skyblue"
+        nx.draw(
+            graph,
+            pos,
+            with_labels=True,
+            labels=labels,
+            node_color=node_colors,
+            node_size=600,
+            font_size=9,
+            edge_color="gray",
+        )
 
-    curr_node_labels = []
-    for n in nodes:
-        curr_node_labels.append(node_labels[n - 1])
-    labels = dict(zip(graph.nodes(), curr_node_labels))
+    else:
+        curr_node_labels = [node_labels[n - 1] for n in nodes]
 
-    pos = nx.spring_layout(graph)
-    nx.draw(graph, pos, with_labels=True, labels=labels)
+        unique_labels = sorted(set(curr_node_labels))
+        color_map = cm.get_cmap("tab10", len(unique_labels))
+        label_to_color = {lbl: color_map(i) for i, lbl in enumerate(unique_labels)}
+        node_colors = [label_to_color[lbl] for lbl in curr_node_labels]
+
+        labels = {n: str(n) for n in nodes}
+        nx.draw(
+            graph,
+            pos,
+            with_labels=True,
+            labels=labels,
+            node_color=node_colors,
+            node_size=600,
+            font_size=9,
+            edge_color="gray",
+        )
+
+        for lbl, color in label_to_color.items():
+            plt.scatter([], [], color=color, label=f"Label {lbl}")
+        plt.legend(title="Node Labels")
+
     plt.show()
