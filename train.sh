0198d0fcb13800d9a502c60405fd265eebea261b
Spiros Maggioros <spirosastro@gmail.com>
2026-01-21 15:51:09 +0200

    [trainer] Fixed available classifiers bug

---
+++ train.sh
@@ -0,0 +1,8 @@
+information_systems train --model graph2vec \
+                          --dataset_dir data/MUTAG \
+                          --test_size 0.25 \
+                          --classifier SVC \
+                          --device mps \
+                          --out_channels 128 \
+                          --epochs 100 \
+                          --model_name graph2vec_mutag_svm.pkl \
