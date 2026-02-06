18b3fdaa055931c0f228a4ee869ff388fe8a7560
Spiros Maggioros <spirosastro@gmail.com>
2026-02-06 14:50:50 +0200

    [Documentation] Added paper and remove unused file, final commit

--- train.sh
+++
@@ -1,9 +0,0 @@
-information_systems train --model graph2vec \
-                          --dataset_dir data/MUTAG \
-                          --test_size 0.25 \
-                          --classifier SVC \
-                          --device mps \
-                          --out_channels 128 \
-                          --epochs 100 \
-                          --model_name graph2vec_mutag_svm.pkl \
-                          --classifier_name svm_mutag_graph2vec.pkl
